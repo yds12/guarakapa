d916547032d04980146a3fdd6162e9ff672c8031
yds12 <28710965+yds12@users.noreply.github.com>
2021-07-13 22:11:02 +0200

    Passes encrypt/decrypt first tests.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,9 +1,11 @@
-fn encrypt(content: String, password: &str) -> String {
-  content
+fn encrypt(content: &[u8], iv: &[u8], password: &[u8]) -> Vec<u8> {
+  openssl::symm::encrypt(openssl::symm::Cipher::aes_256_cbc(), password,
+    Some(iv), content).unwrap()
 }
 
-fn decrypt(secret: String, password: &str) -> String {
-  secret
+fn decrypt(secret: &[u8], iv: &[u8], password: &[u8]) -> Vec<u8> {
+  openssl::symm::decrypt(openssl::symm::Cipher::aes_256_cbc(), password,
+    Some(iv), secret).unwrap()
 }
 
 #[cfg(test)]
@@ -13,20 +15,22 @@ mod tests {
   #[test]
   fn encrypting_and_decrypting_should_retrieve_content() {
     let content = "This is my text.\n\nLet's see if I can retrieve it!.";
-    let pw = "a very strong pw!";
-    let encrypted = encrypt(String::from(content), pw);
-    let decrypted = decrypt(encrypted, pw);
+    let pw = &[0u8; 32][..]; // has to be 32 bytes
+    let iv = &[0u8; 16][..]; // has to be 16 bytes
+    let encrypted = encrypt(content.as_bytes(), iv, pw);
+    let decrypted = decrypt(encrypted.as_slice(), iv, pw);
 
-    assert_eq!(content, decrypted.as_str());
+    assert_eq!(content.as_bytes(), decrypted.as_slice());
   }
 
   #[test]
   fn encrypting_should_yield_something_different() {
     let content = "This is my text.\n\nLet's see if I can retrieve it!.";
-    let pw = "a very strong pw!";
-    let encrypted = encrypt(String::from(content), pw);
+    let pw = &[0u8; 32][..]; // has to be 32 bytes
+    let iv = &[0u8; 16][..]; // has to be 16 bytes
+    let encrypted = encrypt(content.as_bytes(), iv, pw);
 
-    assert!(content != encrypted.as_str());
+    assert!(content.as_bytes() != encrypted.as_slice());
   }
 }
 
