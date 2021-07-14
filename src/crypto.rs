f0ef7db1ee5ac74aee9f0cb26a46688b2f57a2d3
yds12 <28710965+yds12@users.noreply.github.com>
2021-07-14 21:45:59 +0200

    Generates random IV.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 /// Encrypt a message using a key and an initialization vector
 pub fn encrypt(content: &[u8], iv: &[u8], key: &[u8]) -> Vec<u8> {
   openssl::symm::encrypt(openssl::symm::Cipher::aes_256_cbc(), key,
@@ -17,6 +19,15 @@ pub fn derive_key(password: String, salt: &[u8]) -> [u8; 32] {
   hasher.update(password.as_bytes());
   hasher.update(salt);
   hasher.finish()
+}
+
+/// Generates an initialization vector.
+pub fn generate_iv() -> Vec<u8> {
+  let mut iv = [0u8; 16];
+
+  // this RNG is supposed to be cryptographically secure
+  rand::thread_rng().fill_bytes(&mut iv[..]);
+  iv.into()
 }
 
 #[cfg(test)]
@@ -44,6 +55,19 @@ mod tests {
     let encrypted = encrypt(content.as_bytes(), iv, &pw[..]);
 
     assert!(content.as_bytes() != encrypted.as_slice());
+  }
+
+  #[test]
+  fn generated_ivs_are_different() {
+    let mut iv_set_1 = Vec::new();
+    let mut iv_set_2 = Vec::new();
+
+    for _ in 0..20 {
+      iv_set_1.push(generate_iv());
+      iv_set_2.push(generate_iv());
+    }
+
+    assert!(iv_set_1 != iv_set_2);
   }
 }
 
