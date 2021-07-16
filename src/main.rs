2cd38338f0e574590f53f9d08d0c51285f7d5edb
yds12 <28710965+yds12@users.noreply.github.com>
2021-07-16 19:09:06 +0200

    Prompts for password.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use scanpw::scanpw;
 mod crypto;
 
 fn main() {
+  let has_pw = false;
+
+  if !has_pw {
+    let pw = scanpw!(None as Option<char>, "Type your master password: ");
+    println!("");
+    let confirm = scanpw!(None as Option<char>, "Please repeat: ");
+    println!("");
+
+    if pw != confirm {
+      println!("Password confirmation incorrect!");
+    } else {
+      println!("Password saved!");
+    }
+  }
 }
 
