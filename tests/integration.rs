8b226d4be951823d6717875a9ba6759dfaa8acfb
yds12 <28710965+yds12@users.noreply.github.com>
2021-07-31 11:30:42 +0200

    Integration test: retrieve entry.

--- tests/integration.rs
+++ tests/integration.rs
@@ -120,3 +120,34 @@ fn can_add_several_entries() {
   assert!(!fs::file_exists());
 }
 
+#[test]
+fn can_retrieve_entry() {
+  delete_file();
+
+  let original_content = create_file();
+  assert!(fs::file_exists());
+  assert!(original_content.len() > 0);
+
+  let mut file = read_file();
+
+  let pw = String::from(PASSWORD);
+  let pw_hash = crypto::hash(vec![pw.as_bytes(), &file.head.salt[..]]);
+
+  if pw_hash != file.head.pw_hash {
+    panic!("Wrong password hash.");
+  }
+
+  let entry_name = "entry1";
+  let new_content = add_dummy_entry(&mut file, pw, entry_name);
+  assert!(original_content != new_content);
+  assert!(new_content.len() > original_content.len());
+
+  let pw = String::from(PASSWORD);
+  let mut file = read_file();
+  let entry = file.get_entry(pw, entry_name).unwrap().unwrap();
+  assert_eq!(entry, get_dummy_entry());
+
+  delete_file();
+  assert!(!fs::file_exists());
+}
+
