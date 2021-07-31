5c2aca52e4613d9879aa16b22e9efc7de58ff0db
yds12 <28710965+yds12@users.noreply.github.com>
2021-07-31 11:24:13 +0200

    Tests adding several entries.

--- tests/integration.rs
+++ tests/integration.rs
@@ -52,8 +52,9 @@ fn add_dummy_entry(file: &mut fman::File, pw: String, entry_name: &str) -> Vec<u
 fn can_create_file() {
   delete_file();
 
-  create_file();
+  let contents = create_file();
   assert!(fs::file_exists());
+  assert!(contents.len() > 0);
 
   delete_file();
   assert!(!fs::file_exists());
@@ -65,6 +66,7 @@ fn can_add_entry() {
 
   let original_content = create_file();
   assert!(fs::file_exists());
+  assert!(original_content.len() > 0);
 
   let mut file = read_file();
 
@@ -77,6 +79,42 @@ fn can_add_entry() {
 
   let new_content = add_dummy_entry(&mut file, pw, "entry1");
   assert!(original_content != new_content);
+  assert!(new_content.len() > original_content.len());
+
+  delete_file();
+  assert!(!fs::file_exists());
+}
+
+#[test]
+fn can_add_several_entries() {
+  delete_file();
+
+  let original_content = create_file();
+  assert!(fs::file_exists());
+  assert!(original_content.len() > 0);
+
+  let mut file = read_file();
+
+  let pw = String::from(PASSWORD);
+  let pw_hash = crypto::hash(vec![pw.as_bytes(), &file.head.salt[..]]);
+
+  if pw_hash != file.head.pw_hash {
+    panic!("Wrong password hash.");
+  }
+
+  let new_content = add_dummy_entry(&mut file, pw, "entry1");
+  assert!(original_content != new_content);
+  assert!(new_content.len() > original_content.len());
+
+  let pw = String::from(PASSWORD);
+  let newer_content = add_dummy_entry(&mut file, pw, "entry2");
+  assert!(new_content != newer_content);
+  assert!(newer_content.len() > new_content.len());
+
+  let pw = String::from(PASSWORD);
+  let newest_content = add_dummy_entry(&mut file, pw, "entry3");
+  assert!(newer_content != newest_content);
+  assert!(newest_content.len() > newer_content.len());
 
   delete_file();
   assert!(!fs::file_exists());
