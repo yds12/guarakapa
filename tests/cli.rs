5e996a4921d6f5a5b5c3e8a2e789c9e1eb12fd2e
yds12 <28710965+yds12@users.noreply.github.com>
2021-11-19 22:10:06 +0100

    CLI tests for entry add and non-empty ls

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,6 @@
 use rexpect::{spawn, process::wait::WaitStatus};
 
+const TIMEOUT: u64 = 1_000;
 const EXE: &str = env!("CARGO_BIN_EXE_kapa");
 const TEST_PW: &str = "test-password";
 const WRONG_PW: &str = "not-the-password";
@@ -39,7 +40,7 @@ fn ensure_file_is_deleted() {
 }
 
 fn create_file() {
-  let mut p = spawn(EXE, Some(1_000)).unwrap();
+  let mut p = spawn(EXE, Some(TIMEOUT)).unwrap();
   p.exp_regex("password").unwrap();
   p.send_line(TEST_PW).unwrap();
   p.exp_regex("repeat").unwrap();
@@ -48,9 +49,26 @@ fn create_file() {
   assert!(file_exists());
 }
 
+fn add_entry(name: &str) {
+  let mut p = spawn(&format!("{} {} {}", EXE, "add", name), Some(TIMEOUT)).unwrap();
+  p.exp_regex("password").unwrap();
+  p.send_line(TEST_PW).unwrap();
+  p.exp_regex("description").unwrap();
+  p.send_line("some desc").unwrap();
+  p.exp_regex("user name").unwrap();
+  p.send_line("some name").unwrap();
+  p.exp_regex("email").unwrap();
+  p.send_line("some email").unwrap();
+  p.exp_regex("observations").unwrap();
+  p.send_line("some obs").unwrap();
+  p.exp_regex("new password").unwrap();
+  p.send_line("some pw").unwrap();
+  p.exp_regex("added").unwrap();
+}
+
 #[test]
 fn can_execute() {
-  let p = spawn(&format!("{} {}", EXE, "-v"), Some(1_000)).unwrap();
+  let p = spawn(&format!("{} {}", EXE, "-v"), Some(TIMEOUT)).unwrap();
   match p.process.wait() {
     Ok(WaitStatus::Exited(_, 0)) => (),
     _ => panic!("process exited with non-zero status")
@@ -59,7 +77,7 @@ fn can_execute() {
 
 #[test]
 fn can_display_help_text() {
-  let mut p = spawn(&format!("{} {}", EXE, "--help"), Some(1_000)).unwrap();
+  let mut p = spawn(&format!("{} {}", EXE, "--help"), Some(TIMEOUT)).unwrap();
   p.exp_string("usage").unwrap();
 }
 
@@ -68,7 +86,7 @@ fn can_display_version() {
   let version_opts = vec!["-v", "--version", "version"];
 
   for version in version_opts {
-    let mut p = spawn(&format!("{} {}", EXE, version), Some(1_000)).unwrap();
+    let mut p = spawn(&format!("{} {}", EXE, version), Some(TIMEOUT)).unwrap();
     p.exp_string(env!("CARGO_PKG_VERSION")).unwrap();
   }
 }
@@ -79,7 +97,7 @@ fn reports_file_missing() {
   let opts = vec!["ls", "add myentry", "rm myentry", "entry"];
 
   for opt in opts {
-    let mut p = spawn(&format!("{} {}", EXE, opt), Some(1_000)).unwrap();
+    let mut p = spawn(&format!("{} {}", EXE, opt), Some(TIMEOUT)).unwrap();
     p.exp_regex("not found").unwrap();
   }
 }
@@ -94,7 +112,7 @@ fn can_create_data_file() {
 #[test]
 fn cannot_create_file_without_confirming_pw() {
   ensure_file_is_deleted();
-  let mut p = spawn(EXE, Some(1_000)).unwrap();
+  let mut p = spawn(EXE, Some(TIMEOUT)).unwrap();
   p.exp_regex("password").unwrap();
   p.send_line(TEST_PW).unwrap();
   p.exp_regex("repeat").unwrap();
@@ -107,7 +125,7 @@ fn cannot_create_file_without_confirming_pw() {
 fn can_show_path_to_file() {
   ensure_file_is_deleted();
   create_file();
-  let mut p = spawn(&format!("{} {}", EXE, "path"), Some(1_000)).unwrap();
+  let mut p = spawn(&format!("{} {}", EXE, "path"), Some(TIMEOUT)).unwrap();
   p.exp_regex(&get_file_path()).unwrap_or_fail();
   ensure_file_is_deleted();
 }
@@ -116,7 +134,7 @@ fn can_show_path_to_file() {
 fn displays_pw_error_for_listing() {
   ensure_file_is_deleted();
   create_file();
-  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(1_000)).unwrap();
+  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(TIMEOUT)).unwrap();
   p.send_line(WRONG_PW).unwrap();
   p.exp_regex("Error retrieving entries").unwrap();
   ensure_file_is_deleted();
@@ -126,9 +144,50 @@ fn displays_pw_error_for_listing() {
 fn can_list_zero_entries() {
   ensure_file_is_deleted();
   create_file();
-  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(1_000)).unwrap();
+  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(TIMEOUT)).unwrap();
   p.send_line(TEST_PW).unwrap();
   p.exp_regex("no entries yet").unwrap();
+  ensure_file_is_deleted();
+}
+
+#[test]
+fn can_add_entry() {
+  ensure_file_is_deleted();
+  create_file();
+  add_entry("entry1");
+  ensure_file_is_deleted();
+}
+
+#[test]
+fn can_add_two_entries() {
+  ensure_file_is_deleted();
+  create_file();
+  add_entry("entry1");
+  add_entry("entry2");
+  ensure_file_is_deleted();
+}
+
+#[test]
+fn can_list_one_entry() {
+  ensure_file_is_deleted();
+  create_file();
+  add_entry("entry1");
+  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(TIMEOUT)).unwrap();
+  p.send_line(TEST_PW).unwrap();
+  p.exp_regex("entry1").unwrap();
+  ensure_file_is_deleted();
+}
+
+#[test]
+fn can_list_two_entries() {
+  ensure_file_is_deleted();
+  create_file();
+  add_entry("entry1");
+  add_entry("entry2");
+  let mut p = spawn(&format!("{} {}", EXE, "ls"), Some(TIMEOUT)).unwrap();
+  p.send_line(TEST_PW).unwrap();
+  p.exp_regex("entry1").unwrap();
+  p.exp_regex("entry2").unwrap();
   ensure_file_is_deleted();
 }
 
